b2d36f2b30f4201ab5961be9430eae364f4acd0e
José Raphael de Oliveira Nascimento <87425929+RaphaelDNascimento@users.noreply.github.com>
2021-10-11 20:57:49 -0300

    b

--- onepiece/onepiece.js
+++ onepiece/onepiece.js
@@ -1,7 +1,10 @@
-var noh = new Date();
-var nouh2 = noh.getHours();
-var seco = noh.getSeconds();
-var minu = noh.getMinutes();
-var res = document.querySelector('#res');
-res.innerHTML=`${nouh2}:${minu}:${sec}`;
+var noh = new Date()
+var nouh2 = noh.getHours()
+var seco = noh.getSeconds()
+var minu = noh.getMinutes()
+var res = document.getElementById('res')
+var data = noh.getDate()
+var mes = noh.getMonth()
+res.innerHTML=` <p>${nouh2}:${minu}:${seco}</p> <p>${data}/${mes}</p> `
+
 
